43477aff5c4b7f8a22049881068e14ed72ae6f0d
“CindyS” <veggiebyte@yahoo.com>
2026-01-15 15:56:39 -0800

    Final

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,21 +1,36 @@
-// src/components/Dashboard/Dashboard.jsx
-
-import { useContext } from 'react';
-
+import { useEffect, useContext, useState } from 'react';
 import { UserContext } from '../../contexts/UserContext';
+import * as userService from '../../services/userService';
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const fetchedUsers = await userService.index();
+        setUsers(fetchedUsers);
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    if (user) fetchUsers();
+  }, [user]);
 
   return (
     <main>
       <h1>Welcome, {user.username}</h1>
-      <p>
-        This is the dashboard page where you can see a list of all the users.
-      </p>
+      <p>This is the dashboard page where you can see a list of all the users.</p>
+      
+      <h2>Users:</h2>
+      <ul>
+        {users.map((userItem) => (
+          <li key={userItem._id}>{userItem.username}</li>
+        ))}
+      </ul>
     </main>
   );
 };
 
 export default Dashboard;
-
