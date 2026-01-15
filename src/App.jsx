6c501ef2ccaa3a9d0e53484f0b3ebdbe512771f0
“CindyS” <veggiebyte@yahoo.com>
2026-01-15 11:13:00 -0800

    Build Sign Up Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+// src/App.jsx
+
+import { Routes, Route } from 'react-router'; // Import React Router
+
 import NavBar from './components/NavBar/NavBar';
+// Import the SignUpForm component
+import SignUpForm from './components/SignUpForm/SignUpForm';
 
 const App = () => {
-  
+
   return (
     <>
       <NavBar />
-      <h1>Hello, friend!</h1>
+      {/* Add the Routes component to wrap our individual routes*/}
+      <Routes>
+        <Route path='/sign-up' element={<SignUpForm />} />
+      </Routes>
     </>
   );
 };
 
 export default App;
+
